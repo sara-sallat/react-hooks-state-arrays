e291eb144e0324a162edb7cd4c218764c186e35b
sara sallat <sallatsara@gmail.com>
2022-12-10 21:00:08 +0300

    completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,24 +1,74 @@
 import React, { useState } from "react";
-import { spicyFoods, getNewRandomSpicyFood } from "../data";
+import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
 
   function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFood = getNewSpicyFood();
+    if (newFood === undefined) return;
+    setFoods([...foods, newFood]);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  const handleRemoveFood = (foodId) => {
+    setFoods(foods.filter((food) => food.id !== foodId));
+  };
+
+  const handleIncrementHeat = (foodId) => {
+    setFoods(
+      foods.map((food) => {
+        if (food.id === foodId) {
+          return { ...food, heatLevel: food.heatLevel + 1 };
+        }
+        return food;
+      })
+    );
+  };
+
+  const handleFitlerChange = (e) => {
+    setFilterBy(e.target.value);
+  };
+
+  const foodToDisplay = foods.filter((food) => {
+    if (filterBy === "All") return true;
+    return filterBy === food.cuisine;
+  });
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+      <select name="filter" onChange={handleFitlerChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+      <ul>
+        {foodToDisplay.map((food) => {
+          return (
+            <li key={food.id} style={{ display: "flex" }}>
+              <div>
+                {food.name}
+                <span>{`, Cuisine: ${food.cuisine}, HeatLevel: ${food.heatLevel}`}</span>
+                <button
+                  onClick={() => handleIncrementHeat(food.id)}
+                  style={{ marginLeft: 5 }}
+                >
+                  ⬆️
+                </button>
+              </div>
+              <button
+                style={{ marginLeft: 5 }}
+                onClick={() => handleRemoveFood(food.id)}
+              >
+                🗑️
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
